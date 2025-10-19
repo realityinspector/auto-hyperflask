846696fd906679c2c9feaef967dad728f44babe9
realityinspector <mcdonald.sean@gmail.com>
2025-10-19 09:28:55 -0700

    Add all Playwright library paths to LD_LIBRARY_PATH in shell.nix

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ pkgs.mkShell {
       source venv/bin/activate
     fi
 
-    # Set library path for compiled Python extensions
-    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+    # Set library path for compiled Python extensions and Playwright
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.xorg.libX11}/lib:${pkgs.systemd}/lib:${pkgs.glib.out}/lib:${pkgs.nss.out}/lib:${pkgs.nspr.out}/lib:${pkgs.atk.out}/lib:${pkgs.at-spi2-atk.out}/lib:${pkgs.cups.out}/lib:${pkgs.dbus.lib}/lib:${pkgs.xorg.libXcomposite.out}/lib:${pkgs.xorg.libXdamage.out}/lib:${pkgs.xorg.libXext.out}/lib:${pkgs.xorg.libXfixes.out}/lib:${pkgs.xorg.libXrandr.out}/lib:${pkgs.libxkbcommon.out}/lib:${pkgs.mesa.out}/lib:${pkgs.expat.out}/lib:${pkgs.alsa-lib.out}/lib:${pkgs.pango.out}/lib:${pkgs.cairo.out}/lib:$LD_LIBRARY_PATH"
   '';
 }
